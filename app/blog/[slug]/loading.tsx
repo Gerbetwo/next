e75fe87e2b5e71d0ce33c85368f1d1a68d43f427
Daniel Alejandro Lasso Molina <lassomoluna9@gmail.com>
2026-05-27 07:36:47 -0500

    fix: Now using slug configuration

---
+++ app/blog/[slug]/loading.tsx
@@ -0,0 +1,32 @@
+export default function Loading() {
+  return (
+    <main>
+      <div
+        style={{
+          width: "60%",
+          height: 28,
+          background: "#e5e7eb",
+          borderRadius: 4,
+          marginBottom: 16,
+        }}
+      />
+      <div
+        style={{
+          width: "100%",
+          height: 14,
+          background: "#e5e7eb",
+          borderRadius: 4,
+          marginBottom: 8,
+        }}
+      />
+      <div
+        style={{
+          width: "85%",
+          height: 14,
+          background: "#e5e7eb",
+          borderRadius: 4,
+        }}
+      />
+    </main>
+  );
+}
